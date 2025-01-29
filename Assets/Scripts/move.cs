e03b8cd5256897d5ed808c14d1456f77c97b14d6
ozn1911 <117126115+ozn1911@users.noreply.github.com>
2025-01-29 23:53:13 +0300

    .

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class move : MonoBehaviour
 {
     public bool canMove = true;
-
+    Rigidbody2D rb;
 
     public void cantmove()
     {
@@ -14,7 +14,7 @@ public class move : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();   
     }
 
     // Update is called once per frame
@@ -22,7 +22,11 @@ public class move : MonoBehaviour
     {
         if (canMove)
         {
-            transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0).normalized * Time.deltaTime;
+            rb.velocity = (new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0).normalized * 5);
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
         }
     }
 }
